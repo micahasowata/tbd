02e24ed23a3cf08d42383baa4814590eae7e84b3
Micah Asowata <andrewgary963@gmail.com>
2024-02-23 23:52:27 +0000

    test: ensure password verifier behavior

--- pkg/utils/password_test.go
+++ pkg/utils/password_test.go
@@ -11,20 +11,23 @@ func TestHashPassword(t *testing.T) {
 
 	// Length of a normal bcrypt hash is 60
 	if len(hash) != 60 {
-		t.Error("Hash should be 60 chracters long")
+		t.Error("hash should be 60 chracters long")
 	}
 
 	if string(hash) == plaintext {
-		t.Error("Hash should not be the same as plaintext")
+		t.Error("hash should not be the same as plaintext")
 	}
 }
 
 func TestCheckPassword(t *testing.T) {
-	password := "password"
-	hashed, err := HashPassword(password)
+	plaintext := "password"
+	hash, err := HashPassword(plaintext)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	err = CheckPassword(pa)
+	err = CheckPassword(hash, plaintext)
+	if err != nil {
+		t.Errorf("password verification failed %s", err.Error())
+	}
 }
