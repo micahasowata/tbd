0e7bfcb54409cb23e07419cc8718520e4973b977
Micah Asowata <andrewgary963@gmail.com>
2024-02-24 16:37:13 +0000

    feat: add method to connect to database

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/micahasowata/tbd
 
 go 1.22.0
 
-require golang.org/x/crypto v0.19.0
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/lib/pq v1.10.9
+	golang.org/x/crypto v0.19.0
+)
---
+++ pkg/store/db.go
@@ -0,0 +1,26 @@
+package store
+
+import (
+	"database/sql"
+	"time"
+
+	_ "github.com/lib/pq"
+)
+
+func New(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetMaxOpenConns(25)
+	db.SetMaxIdleConns(25)
+	db.SetConnMaxIdleTime(5 * time.Minute)
+
+	return db, nil
+}
