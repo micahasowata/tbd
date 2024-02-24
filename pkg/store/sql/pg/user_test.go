9bf069494efb6f9fcd22a18d4b8c65221754c10b
Micah Asowata <andrewgary963@gmail.com>
2024-02-24 19:05:09 +0000

    test: ensure proper behavior for deleting user

--- pkg/store/sql/pg/user_test.go
+++ pkg/store/sql/pg/user_test.go
@@ -59,3 +59,45 @@ func TestCreateUser(t *testing.T) {
 		t.Errorf("password hash must be hashed")
 	}
 }
+
+func TestDeleteUser(t *testing.T) {
+	// Arrange
+	db, err := store.NewTestDB()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	store := NewPGStore(db)
+
+	input := struct {
+		Name     string
+		Email    string
+		Password string
+	}{
+		Name:     "John Doe",
+		Email:    fmt.Sprintf("test-%s@test.com", xid.New().String()),
+		Password: "password",
+	}
+
+	hash, err := utils.HashPassword(input.Password)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	user := &domain.User{
+		Name:     input.Name,
+		Email:    input.Email,
+		Password: hash,
+	}
+
+	user, err = store.CreateUser(user)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	//Act
+	err = store.DeleteUser(user.ID)
+	if err != nil {
+		t.Errorf("Expected nil, got %q", err)
+	}
+}
