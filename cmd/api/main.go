3cb9dced106dd4c3a47547375e3e9cdc9bcc2e47
Micah Asowata <andrewgary963@gmail.com>
2024-02-24 16:37:34 +0000

    feat: connect to the database

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "log/slog"
+import (
+	"log/slog"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/micahasowata/tbd/pkg/store"
+)
 
 func main() {
+	err := godotenv.Load(".envrc")
+	if err != nil {
+		panic(err)
+	}
+
+	db, err := store.New(os.Getenv("DB_DSN"))
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
 	slog.Info("Hello world!")
 }
