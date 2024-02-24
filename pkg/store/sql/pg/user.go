b58f7d6d4c3b1af975bc4080ac397147c4fe41c1
Micah Asowata <andrewgary963@gmail.com>
2024-02-24 18:27:39 +0000

    refactor: remove hashing from the user create flow

--- pkg/store/sql/pg/user.go
+++ pkg/store/sql/pg/user.go
@@ -2,7 +2,6 @@ package pg
 
 import (
 	"github.com/micahasowata/tbd/pkg/domain"
-	"github.com/micahasowata/tbd/pkg/utils"
 )
 
 func (s *PGStore) CreateUser(user *domain.User) (*domain.User, error) {
@@ -11,25 +10,18 @@ func (s *PGStore) CreateUser(user *domain.User) (*domain.User, error) {
 	VALUES ($1, $2, $3)
 	RETURNING id, name, email, password`
 
-	hash, err := utils.HashPassword(user.Password)
-	if err != nil {
-		return nil, err
-	}
+	args := []any{user.Name, user.Email, user.Password}
 
-	args := []any{user.Name, user.Email, hash}
-
-	err = s.db.QueryRow(query, args...).Scan(
+	err := s.db.QueryRow(query, args...).Scan(
 		&user.ID,
 		&user.Name,
 		&user.Email,
-		&hash,
+		&user.Password,
 	)
 
 	if err != nil {
 		return nil, err
 	}
-
-	user.Password = string(hash)
 
 	return user, nil
 }
