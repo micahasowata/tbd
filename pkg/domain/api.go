9d0a5ae2aa8d1adadbb5dde237d2d1883b8cffbf
Micah Asowata <andrewgary963@gmail.com>
2024-02-24 18:26:52 +0000

    refactor: update the `User` struct for better DX
    - arrange the field names to match the `users` table column ordering
    - make the `Password` field a byte slice to match the `bytea` type on the db
    - prevent it from being marshalled to JSON (to mitigate password leak risk)

--- pkg/domain/api.go
+++ pkg/domain/api.go
@@ -4,9 +4,9 @@ import "time"
 
 type User struct {
 	ID       int    `json:"id"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
 	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password []byte `json:"-"`
 }
 
 type Post struct {
