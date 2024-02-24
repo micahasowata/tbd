63017c6536982e694896958bde1f0b6de7f7ce48
Micah Asowata <andrewgary963@gmail.com>
2024-02-24 17:27:27 +0000

    feat: complete user creation functionality

--- pkg/store/sql/pg/user.go
+++ pkg/store/sql/pg/user.go
@@ -1,8 +1,35 @@
 package pg
 
-import "github.com/micahasowata/tbd/pkg/domain"
+import (
+	"github.com/micahasowata/tbd/pkg/domain"
+	"github.com/micahasowata/tbd/pkg/utils"
+)
 
 func (s *PGStore) CreateUser(user *domain.User) (*domain.User, error) {
-	user.ID = 1
+	query := `
+	INSERT INTO users (name, email, password)
+	VALUES ($1, $2, $3)
+	RETURNING id, name, email, password`
+
+	hash, err := utils.HashPassword(user.Password)
+	if err != nil {
+		return nil, err
+	}
+
+	args := []any{user.Name, user.Email, hash}
+
+	err = s.db.QueryRow(query, args...).Scan(
+		&user.ID,
+		&user.Name,
+		&user.Email,
+		&hash,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	user.Password = string(hash)
+
 	return user, nil
 }
