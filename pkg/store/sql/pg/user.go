41781dad3ef03bb28a7ef35697ce7e6df9005ebd
Micah Asowata <andrewgary963@gmail.com>
2024-02-24 20:58:47 +0000

    feat: add functionality to get user data by email

--- pkg/store/sql/pg/user.go
+++ pkg/store/sql/pg/user.go
@@ -1,7 +1,17 @@
 package pg
 
 import (
+	"database/sql"
+	"errors"
+	"strings"
+
 	"github.com/micahasowata/tbd/pkg/domain"
+)
+
+var (
+	DuplicateData   = "pq: duplicate key value violates unique constraint"
+	ErrEmailInUse   = errors.New("email is already in use")
+	ErrUserNotFound = errors.New("user not found")
 )
 
 func (s *PGStore) CreateUser(user *domain.User) (*domain.User, error) {
@@ -20,7 +30,38 @@ func (s *PGStore) CreateUser(user *domain.User) (*domain.User, error) {
 	)
 
 	if err != nil {
-		return nil, err
+		switch {
+		case strings.Contains(err.Error(), DuplicateData):
+			return nil, ErrEmailInUse
+		default:
+			return nil, err
+		}
+	}
+
+	return user, nil
+}
+
+func (s *PGStore) GetUserByEmail(email string) (*domain.User, error) {
+	query := `SELECT id, name, email, password
+	FROM users
+	WHERE email = $1`
+
+	user := &domain.User{}
+
+	err := s.db.QueryRow(query, email).Scan(
+		&user.ID,
+		&user.Name,
+		&user.Email,
+		&user.Password,
+	)
+
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return nil, ErrUserNotFound
+		default:
+			return nil, err
+		}
 	}
 
 	return user, nil
@@ -31,9 +72,18 @@ func (s *PGStore) DeleteUser(id int) error {
 	DELETE FROM users
 	WHERE id = $1`
 
-	_, err := s.db.Exec(query, id)
+	result, err := s.db.Exec(query, id)
 	if err != nil {
 		return err
+	}
+
+	rows, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if rows != 1 {
+		return ErrUserNotFound
 	}
 
 	return nil
