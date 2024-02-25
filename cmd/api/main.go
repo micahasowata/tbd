5d5e2098834a9adc92b71f204dc6288659e9ab4b
Micah Asowata <andrewgary963@gmail.com>
2024-02-25 13:27:07 +0000

    feat: set up application server

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"log/slog"
+	"net/http"
 	"os"
 
 	"github.com/joho/godotenv"
 	"github.com/micahasowata/tbd/pkg/store"
 )
+
+type server struct {
+	logger *slog.Logger
+}
 
 func main() {
 	err := godotenv.Load(".envrc")
@@ -14,11 +19,30 @@ func main() {
 		panic(err)
 	}
 
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+
 	db, err := store.New(os.Getenv("DB_DSN"))
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
 
-	slog.Info("Hello world!")
+	logger.Info("db connected successfully")
+
+	s := &server{
+		logger: logger,
+	}
+
+	srv := &http.Server{
+		Addr:     ":8000",
+		Handler:  s.routes(),
+		ErrorLog: slog.NewLogLogger(logger.Handler(), slog.LevelError),
+	}
+
+	logger.Info("server started", slog.String("port", srv.Addr))
+
+	err = srv.ListenAndServe()
+	if err != nil {
+		logger.Error(err.Error())
+	}
 }
