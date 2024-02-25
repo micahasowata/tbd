2f0cacbd66be9b377a61ad141112a53a880cf9b7
Micah Asowata <andrewgary963@gmail.com>
2024-02-25 15:49:49 +0000

    refactor: ensure `Store` interface compliance

--- pkg/store/sql/pg/pg.go
+++ pkg/store/sql/pg/pg.go
@@ -1,10 +1,16 @@
 package pg
 
-import "database/sql"
+import (
+	"database/sql"
+
+	"github.com/micahasowata/tbd/pkg/domain"
+)
 
 type PGStore struct {
 	db *sql.DB
 }
+
+var _ domain.Store = (*PGStore)(nil)
 
 func NewPGStore(db *sql.DB) *PGStore {
 	return &PGStore{
