b8c52300e179cb6f04877c539d0e074478d1daf8
Micah Asowata <andrewgary963@gmail.com>
2024-02-25 17:12:21 +0000

    test: ensure the create user handler works fine

---
+++ cmd/api/users_test.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"log/slog"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/brianvoe/gofakeit/v7"
+	"github.com/micahasowata/jason"
+	"github.com/micahasowata/tbd/pkg/store"
+	"github.com/micahasowata/tbd/pkg/store/sql/pg"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreateUser(t *testing.T) {
+	body := fmt.Sprintf(`{"name":%s, "email":%s, "password":%s}`,
+		gofakeit.Name(), gofakeit.Email(), gofakeit.Password(true, true, true, false, false, 14))
+
+	db, err := store.NewTestDB()
+	require.Nil(t, err)
+
+	srv := &server{
+		logger: slog.Default(),
+		store:  pg.New(db),
+	}
+
+	ts := httptest.NewServer(srv.routes())
+	defer ts.Close()
+
+	res, err := ts.Client().Post(ts.URL+"/v1/users/create", jason.ContentTypeJSON, strings.NewReader(body))
+	require.Nil(t, err)
+
+	assert.Equal(t, http.StatusOK, res.StatusCode)
+	assert.HTTPBodyContains(t, srv.createUser, http.MethodPost, "/v1/users/create", nil, "OK")
+}
