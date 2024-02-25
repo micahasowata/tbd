778d34baa7f0bf9db391924ecaa998d26d090266
Micah Asowata <andrewgary963@gmail.com>
2024-02-25 19:27:04 +0000

    feat: add a `DeleteAllUsers` method the contract

--- pkg/domain/store.go
+++ pkg/domain/store.go
@@ -3,6 +3,7 @@ package domain
 type Store interface {
 	CreateUser(*User) (*User, error)
 	DeleteUser(int) error
+	DeleteAllUsers() error
 	GetUserByEmail(string) (*User, error)
 	GetUserByID(int) (*User, error)
 }
