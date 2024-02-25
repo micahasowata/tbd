ba0b75cd86fc567a583c62191b0264c196b2aba5
Micah Asowata <andrewgary963@gmail.com>
2024-02-25 18:04:57 +0000

    refactor: read JSON body and handle read error

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -1,7 +1,23 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/micahasowata/jason"
+)
 
 func (s *server) createUser(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte(http.StatusText(http.StatusOK)))
+	var input struct {
+		Name     string `json:"name"`
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	err := s.Read(w, r, &input)
+	if err != nil {
+		s.Write(w, http.StatusBadRequest, jason.Envelope{"error": err.Error()}, nil)
+		return
+	}
+
+	s.Write(w, http.StatusOK, jason.Envelope{"user": input}, nil)
 }
