7198ad9c3475ed523a9e4182e59299ca6c81cc7c
Micah Asowata <andrewgary963@gmail.com>
2024-02-26 02:28:25 +0000

    test: keep test server deps in sync with prod

--- cmd/api/setup_test.go
+++ cmd/api/setup_test.go
@@ -8,18 +8,22 @@ import (
 
 	"github.com/go-playground/validator/v10"
 	"github.com/micahasowata/jason"
+	"github.com/micahasowata/tbd/pkg/security"
 	"github.com/micahasowata/tbd/pkg/store"
 	"github.com/micahasowata/tbd/pkg/store/sql/pg"
 )
 
 func setUpTest() (*server, *httptest.Server) {
 	db, _ := store.NewTestDB()
+	token, _ := security.NewToken([]byte("=9Ha*2tME_-?xPJ_e57PEaF~UfHg6sD,"))
 
 	srv := &server{
-		Jason:    jason.New(100, false, true),
+		Jason: jason.New(100, false, true),
+
 		logger:   slog.Default(),
 		validate: validator.New(validator.WithRequiredStructEnabled()),
 		store:    pg.New(db),
+		tokens:   token,
 	}
 
 	ts := httptest.NewServer(srv.routes())
