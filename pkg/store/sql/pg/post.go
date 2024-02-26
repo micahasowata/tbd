4f126d0b0a89634bee6da8e8665d2a39de73b53a
Micah Asowata <andrewgary963@gmail.com>
2024-02-26 15:47:52 +0000

    feat: complete method to create new post

--- pkg/store/sql/pg/post.go
+++ pkg/store/sql/pg/post.go
@@ -3,7 +3,26 @@ package pg
 import "github.com/micahasowata/tbd/pkg/domain"
 
 func (s *PGStore) CreatePost(post *domain.Post) (*domain.Post, error) {
-	return nil, nil
+	query := `
+	INSERT INTO posts (user_id,title, body)
+	VALUES ($1, $2, $3)
+	RETURNING id, created_at, user_id, title, body`
+
+	args := []any{post.UserID, post.Title, post.Body}
+
+	err := s.db.QueryRow(query, args...).Scan(
+		&post.ID,
+		&post.CreatedAt,
+		&post.UserID,
+		&post.Title,
+		&post.Body,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return post, nil
 }
 func (s *PGStore) GetUserPosts(userID int) ([]*domain.Post, error) {
 	return nil, nil
