faf8ee55b05a182fc0d8ce65849dcab479c84627
Micah Asowata <andrewgary963@gmail.com>
2024-02-26 23:50:44 +0000

    feat: add route and handler to view post

--- cmd/api/posts.go
+++ cmd/api/posts.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"errors"
 	"net/http"
+	"strconv"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/micahasowata/jason"
 	"github.com/micahasowata/tbd/pkg/domain"
+	"github.com/micahasowata/tbd/pkg/store/sql/pg"
 )
 
 func (s *server) createPost(w http.ResponseWriter, r *http.Request) {
@@ -41,6 +45,40 @@ func (s *server) createPost(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		s.logger.Error(err.Error())
 		s.Write(w, http.StatusInternalServerError, jason.Envelope{"error": "request could no longer be processed"}, nil)
+		return
+	}
+
+	s.Write(w, http.StatusOK, jason.Envelope{"post": post}, nil)
+}
+
+func (s *server) getPost(w http.ResponseWriter, r *http.Request) {
+	userID, ok := r.Context().Value(userID).(int)
+	if !ok {
+		s.Write(w, http.StatusBadRequest, jason.Envelope{"error": "invalid request"}, nil)
+		return
+	}
+
+	id := chi.URLParam(r, "id")
+
+	postID, err := strconv.Atoi(id)
+	if err != nil {
+		s.Write(w, http.StatusUnprocessableEntity, jason.Envelope{"error": "invalid id"}, nil)
+		return
+	}
+
+	post := &domain.Post{
+		UserID: userID,
+		ID:     postID,
+	}
+
+	post, err = s.store.GetPost(post)
+	if err != nil {
+		switch {
+		case errors.Is(err, pg.ErrPostNotFound):
+			s.Write(w, http.StatusNotFound, jason.Envelope{"error": "post not found"}, nil)
+		default:
+			s.Write(w, http.StatusInternalServerError, jason.Envelope{"error": "request could no longer be processed"}, nil)
+		}
 		return
 	}
 
--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -13,6 +13,7 @@ func (s *server) routes() http.Handler {
 	router.Post("/v1/users/login", s.loginUser)
 
 	router.With(s.authUser).Post("/v1/posts/create", s.createPost)
+	router.With(s.authUser).Get("/v1/posts/{id}", s.getPost)
 
 	return router
 }
