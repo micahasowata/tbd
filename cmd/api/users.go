09728c2a0a33c2e26c54c0afde3895aa3713d1c1
Micah Asowata <andrewgary963@gmail.com>
2024-02-26 10:36:30 +0000

    feat: add the user login feature

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/micahasowata/jason"
 	"github.com/micahasowata/tbd/pkg/domain"
@@ -51,5 +52,45 @@ func (s *server) createUser(w http.ResponseWriter, r *http.Request) {
 }
 
 func (s *server) loginUser(w http.ResponseWriter, r *http.Request) {
+	var input struct {
+		Email    string `json:"email" validate:"required,email"`
+		Password string `json:"password" validate:"required,min=8,max=72"`
+	}
 
+	err := s.Read(w, r, &input)
+	if err != nil {
+		s.Write(w, http.StatusBadRequest, jason.Envelope{"error": err.Error()}, nil)
+		return
+	}
+
+	err = s.validate.Struct(&input)
+	if err != nil {
+		s.Write(w, http.StatusUnprocessableEntity, jason.Envelope{"error": err.Error()}, nil)
+		return
+	}
+
+	user, err := s.store.GetUserByEmail(input.Email)
+	if err != nil {
+		s.Write(w, http.StatusForbidden, jason.Envelope{"error": "login failed; invalid email or password"}, nil)
+		return
+	}
+
+	err = utils.CheckPassword(user.Password, input.Password)
+	if err != nil {
+		s.logger.Error(err.Error())
+		s.Write(w, http.StatusForbidden, jason.Envelope{"error": "login failed; invalid email or password"}, nil)
+		return
+	}
+
+	t, err := s.tokens.NewJWT(&domain.Claims{ID: user.ID, Email: user.Email}, 3*time.Hour)
+	if err != nil {
+		s.logger.Error(err.Error())
+		s.Write(w, http.StatusInternalServerError, jason.Envelope{"error": "request could not be processed"}, nil)
+		return
+	}
+
+	err = s.Write(w, http.StatusOK, jason.Envelope{"token": t}, nil)
+	if err != nil {
+		return
+	}
 }
