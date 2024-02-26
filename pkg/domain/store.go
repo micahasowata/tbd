4448dace62a82d9973dfd26d839e2a40e94000c6
Micah Asowata <andrewgary963@gmail.com>
2024-02-26 01:50:17 +0000

    feat: set up a JWT interface

--- pkg/domain/store.go
+++ pkg/domain/store.go
@@ -1,9 +1,20 @@
 package domain
 
+import (
+	"time"
+
+	"github.com/micahasowata/tbd/pkg/security"
+)
+
 type Store interface {
 	CreateUser(*User) (*User, error)
 	DeleteUser(int) error
 	DeleteAllUsers() error
 	GetUserByEmail(string) (*User, error)
 	GetUserByID(int) (*User, error)
+}
+
+type JWT interface {
+	NewJWT(*security.Claims, time.Duration) ([]byte, error)
+	VerifyJWT([]byte) (*security.Claims, error)
 }
