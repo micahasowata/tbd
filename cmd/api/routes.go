d18c5e495811b9f12a1a7132a5e9af531672399d
Micah Asowata <andrewgary963@gmail.com>
2024-02-26 09:49:31 +0000

    feat: add user login route

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -7,5 +7,6 @@ import (
 func (s *server) routes() http.Handler {
 	router := http.NewServeMux()
 	router.HandleFunc("POST /v1/users/create", s.createUser)
+	router.HandleFunc("POST /v1/users/login", s.loginUser)
 	return router
 }
