ec19ff6910b9cf75426a2b985904114d21a22e95
Micah Asowata <andrewgary963@gmail.com>
2024-02-26 23:52:00 +0000

    test: ensure post can be retrieved properly

--- cmd/api/posts_test.go
+++ cmd/api/posts_test.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 	"testing"
 
+	"github.com/micahasowata/tbd/pkg/domain"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -57,7 +59,59 @@ func TestCreatePost(t *testing.T) {
 }
 
 func TestGetPost(t *testing.T) {
-	// s, ts := setUpTest()
-	// bearer, user, err := setupAuth(s)
-	// require.Nil(t, err)
+	s, ts := setUpTest()
+	defer s.store.DeleteAllUsers()
+
+	user, err := setupUser(s)
+	require.Nil(t, err)
+
+	post := &domain.Post{
+		UserID: user.ID,
+		Title:  "Hello",
+		Body:   "Body",
+	}
+
+	post, err = s.store.CreatePost(post)
+	require.Nil(t, err)
+
+	tests := []struct {
+		name string
+		path string
+		code int
+	}{
+		{
+			name: "valid",
+			path: ts.URL + "/v1/posts/" + strconv.Itoa(post.ID),
+			code: http.StatusOK,
+		},
+		{
+			name: "invalid id",
+			path: ts.URL + "/v1/posts/" + "abc",
+			code: http.StatusUnprocessableEntity,
+		},
+		{
+			name: "invalid",
+			path: ts.URL + "/v1/posts/" + "56",
+			code: http.StatusNotFound,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			req, err := setUpReq(http.MethodGet, tt.path, "")
+			require.Nil(t, err)
+
+			bearer, err := setupBearer(s.tokens, user)
+			require.Nil(t, err)
+
+			req.Header.Set("Authorization", bearer)
+
+			res, err := ts.Client().Do(req)
+			require.Nil(t, err)
+			require.NotNil(t, res)
+
+			assert.Equal(t, tt.code, res.StatusCode)
+		})
+	}
 }
